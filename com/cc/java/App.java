b996ead44dc0bb20094463f5f63d2b901e6e989f
MathiasWeberComCave <q-mwb@gmx.de>
2022-11-24 18:04:04 +0100

    Mehrfacharry und For-Schleife für Instanzen&Setter

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,13 +4,29 @@ public class App {
     
     public static void main(String[] args) {
      
-    }
+    String [] [] mitarbeiterArray = {{"Henry","Ford","CEO","1890"},{"Bill","Gates","CIO","1989"},{"Elon","Musk","CD","2013"}};
+
+    Mitarbeiter [] mitarbeiter;
+    mitarbeiter = new Mitarbeiter [mitarbeiterArray.length];
+    
+    for (int i = 0; i < mitarbeiterArray.length; i++) {
+        // Erzeuge Instanzen
+        mitarbeiter [i] = new Mitarbeiter(null, null, null, i);
+
+        // Übernehme Werte in die Instanzen
+        mitarbeiter[i].setFirstName(mitarbeiterArray[i][0]);
+        mitarbeiter[i].setFamilyName(mitarbeiterArray[i][1]);
+        mitarbeiter[i].setRole(mitarbeiterArray[i][2]);
+        mitarbeiter[i].setYearOfEntry(Integer.parseInt(mitarbeiterArray[i][3]));
+
+                // Abruf Werte
+                output(mitarbeiter[i].getInfo());
+            }
+       } 
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
-
-
-
 }
+
 
---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,51 @@
+package com.cc.java;
+
+public class Mitarbeiter {
+    private String firstName;
+    private String familyName;
+    private String role;
+    private int yearOfEntry;
+
+    public Mitarbeiter(String firstName, String familyName, String role, int yearOfEntry) {
+        this.firstName = firstName;
+        this.familyName = familyName;
+        this.role = role;
+        this.yearOfEntry = yearOfEntry;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setYearOfEntry(int yearOfEntry) {
+        this.yearOfEntry = yearOfEntry;
+    }
+
+    private String getFirstName() {
+        return firstName;
+    }
+
+    private String getFamilyName() {
+        return familyName;
+    }
+
+    private String getRole() {
+        return role;
+    }
+
+    private int getYearOfEntry() {
+        return yearOfEntry;
+    }
+
+    public String getInfo() {
+        return this.getFirstName() + " " + this.getFamilyName() + " ist als " + this.getRole() + " im Jahr " +  this.getYearOfEntry() + " in das Unternehmen eingetreten.";
+    }   
+}
